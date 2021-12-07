518d08a93fc46c7d6c6f9df1e09fa772c2f35675
danifosy <mail@daniha.de>
2021-12-07 13:52:56 +0100

    Add nav bar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StartMenuComponent } from './start-menu/start-menu.component';
+
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { StartMenuComponent } from './start-menu/start-menu.component';
 import { EyeMenuComponent } from './eye-menu/eye-menu.component';
 import { LipMenuComponent } from './lip-menu/lip-menu.component';
 import { FaceMenuComponent } from './face-menu/face-menu.component';
 import { NailMenuComponent } from './nail-menu/nail-menu.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +24,7 @@ import { NailMenuComponent } from './nail-menu/nail-menu.component';
     LipMenuComponent,
     FaceMenuComponent,
     NailMenuComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,10 @@ import { NailMenuComponent } from './nail-menu/nail-menu.component';
     BrowserAnimationsModule,
     MatGridListModule,
     MatCardModule,
+    MatIconModule,
+    MatMenuModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent],
 })
